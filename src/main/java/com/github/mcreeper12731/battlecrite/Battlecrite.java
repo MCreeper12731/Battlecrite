a1cb0e37250430fa34b04e52c7b4a12417c3ba00
MCreeper12731 <vs97051@student.uni-lj.si>
2023-10-11 22:25:43 +0200

    Initial commit

---
+++ src/main/java/com/github/mcreeper12731/battlecrite/Battlecrite.java
@@ -0,0 +1,13 @@
+package com.github.mcreeper12731.battlecrite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Battlecrite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+}
